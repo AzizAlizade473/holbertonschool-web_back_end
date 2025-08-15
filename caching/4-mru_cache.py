a3bc6d60235b494b000754ce335e1a29eca29cdc
Aziz <elizadeeziz2006@gmail.com>
2025-08-15 18:39:13 +0400

    Add files via upload

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""4-mru_cache module."""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRU caching system."""
+
+    def __init__(self):
+        super().__init__()
+        self.keys_order = []
+
+    def put(self, key, item):
+        """Add an item in the cache."""
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.keys_order.remove(key)
+            self.keys_order.append(key)
+            return
+
+        self.cache_data[key] = item
+        self.keys_order.append(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            mru_key = self.keys_order.pop(-2)
+            del self.cache_data[mru_key]
+            print(f"DISCARD: {mru_key}")
+
+    def get(self, key):
+        """Retrieve an item by key."""
+        if key is None or key not in self.cache_data:
+            return None
+        self.keys_order.remove(key)
+        self.keys_order.append(key)
+        return self.cache_data[key]
