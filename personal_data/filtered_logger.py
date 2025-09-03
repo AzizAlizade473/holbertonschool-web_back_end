8db86682e65f30aa47221ddd9ab21f7268f6cdc4
Aziz <elizadeeziz2006@gmail.com>
2025-09-03 11:42:42 +0400

    Update filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python3
+from typing import List
 import re
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    Returns the log message obfuscated.
+    """
     pattern = f'({"|".join(fields)})=.*?{re.escape(separator)}'
     return re.sub(pattern, lambda m: f'{m.group(1)}={redaction}{separator}', message)
