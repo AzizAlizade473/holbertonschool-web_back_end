ab9eefb6e7c7eff3b952a3242815e63a0ce52b7a
Aziz <elizadeeziz2006@gmail.com>
2025-09-03 11:43:34 +0400

    Update filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,8 +3,6 @@ from typing import List
 import re
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """
-    Returns the log message obfuscated.
-    """
+    
     pattern = f'({"|".join(fields)})=.*?{re.escape(separator)}'
     return re.sub(pattern, lambda m: f'{m.group(1)}={redaction}{separator}', message)
