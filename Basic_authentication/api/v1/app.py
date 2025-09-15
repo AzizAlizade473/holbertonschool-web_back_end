507bceb5478da0463e11ffb4ec4cae580ee869b9
Aziz <elizadeeziz2006@gmail.com>
2025-09-15 22:39:08 +0400

    Update app.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -3,41 +3,23 @@
 Route module for the API
 """
 from os import getenv
-from flask import Flask, jsonify, abort, request
-from flask.ext.cors import CORS
+from flask import Flask, jsonify
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.url_map.strict_slashes = False
-
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
-@app.before_request
-def before_request():
-    """
-    Handles operations before each request.
-    """
-    from api.v1.auth import auth
-    auth = auth.BasicAuth()
-    if auth.require_auth(request.path, ['/api/v1/status/', '/api/v1/unauthorized/']) is False:
-        abort(401)
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
-
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
