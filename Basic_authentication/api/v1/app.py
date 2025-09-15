e43e7ac8117791af0a8594c9507fe8edbf276ada
Aziz <elizadeeziz2006@gmail.com>
2025-09-15 22:32:39 +0400

    Update app.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 from flask import Flask
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    host = os.getenv("API_HOST", "0.0.0.0")
+    port = int(os.getenv("API_PORT", 5000))
+    app.run(host=host, port=port)
