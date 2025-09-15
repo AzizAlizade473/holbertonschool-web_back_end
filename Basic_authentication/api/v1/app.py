fe721791ae17a47af3e7b2828dd6eb1e98e0e31e
Aziz <elizadeeziz2006@gmail.com>
2025-09-15 22:48:06 +0400

    Update app.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -9,20 +9,17 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
-
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
