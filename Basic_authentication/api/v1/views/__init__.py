0ae1ae0011358e46dfec5156def6c39ae0512ae2
Aziz <elizadeeziz2006@gmail.com>
2025-09-15 22:33:09 +0400

    Update __init__.py

--- Basic_authentication/api/v1/views/__init__.py
+++ Basic_authentication/api/v1/views/__init__.py
@@ -2,5 +2,5 @@ from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-from api.v1.views.index import *
-from api.v1.views.users import *
+# Import endpoints so they get registered
+from api.v1.views import index, users
