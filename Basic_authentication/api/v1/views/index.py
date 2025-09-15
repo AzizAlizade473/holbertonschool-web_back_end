d505fb1bfee744018fd5a6b310880fad58533bbe
Aziz <elizadeeziz2006@gmail.com>
2025-09-15 22:48:22 +0400

    Update index.py

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 """
-This module defines the API routes for the index and status endpoints.
-It provides endpoints to check the status of the API and to test the
-error handling for unauthorized requests.
+Module of Index views
 """
-
 from flask import jsonify, abort
 from api.v1.views import app_views
 
@@ -13,15 +10,8 @@ from api.v1.views import app_views
 def status() -> str:
     """
     GET /api/v1/status
-
-    Returns the status of the API.
-
-    This endpoint is used to check if the API is running and
-    responsive. It returns a JSON object with a single key
-    "status" and a value of "OK".
-
-    Returns:
-        A JSON response with the status of the API.
+    Return:
+      - the status of the API
     """
     return jsonify({"status": "OK"})
 
@@ -30,15 +20,7 @@ def status() -> str:
 def unauthorized() -> str:
     """
     GET /api/v1/unauthorized
-
-    Raises a 401 Unauthorized error.
-
-    This endpoint is for testing the custom error handler for
-    the 401 status code. When accessed, it uses Flask's `abort`
-    function to raise an HTTP 401 error, which will trigger the
-    corresponding error handler defined in `app.py`.
-
-    Raises:
-        HTTPException: A 401 Unauthorized error.
+    Return:
+      - raises a 401 error
     """
     abort(401)
