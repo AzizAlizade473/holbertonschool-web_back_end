da787a6885376a9ca11478b74ceb70066a2db3fb
Aziz <elizadeeziz2006@gmail.com>
2025-09-15 22:33:27 +0400

    Update index.py

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,6 +1,6 @@
-from flask import abort
 from api.v1.views import app_views
 
-@app_views.route('/unauthorized', methods=['GET'])
-def unauthorized_route():
-    abort(401)
+@app_views.route("/status", methods=["GET"])
+def status():
+    # Must return plain text "OK" for the checker
+    return "OK"
