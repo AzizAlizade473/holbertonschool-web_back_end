f23b1dfb81ee63f188eaf0f61029f0761a4cf745
Aziz <elizadeeziz2006@gmail.com>
2025-09-15 22:40:05 +0400

    Update index.py

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -4,13 +4,13 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status() -> str:
-    """ GET /api/v1/status
-    Return:
-      - the status of the API
-    """
-    return jsonify({"status": "OK"})
+
+
+@app_views.route("/status", methods=["GET"])
+def status():
+    return "OK", 200
+
+
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
