8745a9c92a2f842e2f47ed09b4eecb0e5c41744e
Aziz <elizadeeziz2006@gmail.com>
2025-09-25 13:41:28 +0400

    Update basic_auth.py

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Basic authentication
+Module for Basic Authentication.
 """
 from api.v1.auth.auth import Auth
 from models.user import User
@@ -8,25 +8,41 @@ from typing import TypeVar
 
 
 class BasicAuth(Auth):
-    """ BasicAuth class
+    """
+    BasicAuth class that inherits from Auth.
+
+    This class is responsible for handling basic authentication.
     """
 
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str
             ) -> TypeVar('User'):
-        """ returns the User instance based on his email and password
+        """
+        Returns the User instance based on his email and password.
+
+        Args:
+            user_email (str): The user's email address.
+            user_pwd (str): The user's password.
+
+        Returns:
+            TypeVar('User'): The User instance if the credentials are valid,
+                             otherwise None.
         """
         if user_email is None or not isinstance(user_email, str):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
+
         try:
             users = User.search({'email': user_email})
         except Exception:
             return None
+
         if not users:
             return None
+
         for user in users:
             if user.is_valid_password(user_pwd):
                 return user
+
         return None
