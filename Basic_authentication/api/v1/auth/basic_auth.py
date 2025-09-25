2c9ee17059fcd7d3758dc41a3dc3526650a91286
Aziz <elizadeeziz2006@gmail.com>
2025-09-25 13:44:53 +0400

    Update basic_auth.py

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,44 +1,38 @@
 #!/usr/bin/env python3
-"""
-Module for Basic Authentication.
+"""Module for Basic Authentication.
 
-This module defines the `BasicAuth` class, which provides
-methods for handling basic authentication.
+This module provides the `BasicAuth` class for handling
+basic authentication in the API.
 """
-
 from api.v1.auth.auth import Auth
 from models.user import User
-from typing import TypeVar
+from typing import TypeVar, Optional
 
 
 class BasicAuth(Auth):
-    """
-    BasicAuth class that inherits from Auth.
+    """BasicAuth class for basic authentication.
 
-    This class is responsible for handling basic authentication mechanisms.
-    It provides methods to extract credentials from requests and retrieve
-    user objects based on those credentials.
+    This class inherits from `Auth` and provides an implementation
+    for a basic authentication system based on user credentials.
     """
 
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str
-            ) -> TypeVar('User'):
-        """
-        Returns the User instance based on their email and password.
+    ) -> Optional[TypeVar('User')]:
+        """Retrieves a User instance based on email and password.
 
-        This method checks for the validity of the provided email and password,
-        searches for a user with the given email, and then verifies
-        if the provided password matches the user's stored password.
+        This method validates the user's credentials against the
+        stored user data.
 
         Args:
-            user_email (str): The email of the user to authenticate.
-            user_pwd (str): The password of the user to authenticate.
+            user_email (str): The email of the user.
+            user_pwd (str): The password of the user.
 
         Returns:
-            TypeVar('User'): The `User` instance if the credentials are valid,
-                             otherwise `None`. Returns `None` if `user_email`
-                             or `user_pwd` are not strings, if no user is found
-                             with the email, or if the password is incorrect.
+            Optional[TypeVar('User')]: The User instance if the credentials
+            are valid, otherwise None. It returns None if `user_email` or
+            `user_pwd` are not strings, if no user is found with the email,
+            or if the password is incorrect.
         """
         if user_email is None or not isinstance(user_email, str):
             return None
@@ -56,5 +50,4 @@ class BasicAuth(Auth):
         for user in users:
             if user.is_valid_password(user_pwd):
                 return user
-
         return None
