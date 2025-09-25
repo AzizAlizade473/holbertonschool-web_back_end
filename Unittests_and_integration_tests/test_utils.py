d1eb2827406fae45e15463e8ebf5e5fabc0e9e24
Aziz <elizadeeziz2006@gmail.com>
2025-09-25 13:48:40 +0400

    Update test_utils.py

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,7 @@ A module for parameterizing a unit test.
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -21,6 +22,17 @@ class TestAccessNestedMap(unittest.TestCase):
         Test that access_nested_map returns the expected result.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        Test that a KeyError is raised for the respective inputs.
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
 
 
 if __name__ == '__main__':
