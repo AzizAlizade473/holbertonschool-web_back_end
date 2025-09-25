5ecc06c0d1ba1ef3121c56c57b4de231d42391f2
Aziz <elizadeeziz2006@gmail.com>
2025-09-25 13:42:10 +0400

    Update basic_auth.py

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 """
 Module for Basic Authentication.
+
+This module defines the `BasicAuth` class, which provides
+methods for handling basic authentication.
 """
+
 from api.v1.auth.auth import Auth
 from models.user import User
 from typing import TypeVar
@@ -11,22 +15,30 @@ class BasicAuth(Auth):
     """
     BasicAuth class that inherits from Auth.
 
-    This class is responsible for handling basic authentication.
+    This class is responsible for handling basic authentication mechanisms.
+    It provides methods to extract credentials from requests and retrieve
+    user objects based on those credentials.
     """
 
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str
             ) -> TypeVar('User'):
         """
-        Returns the User instance based on his email and password.
+        Returns the User instance based on their email and password.
+
+        This method checks for the validity of the provided email and password,
+        searches for a user with the given email, and then verifies
+        if the provided password matches the user's stored password.
 
         Args:
-            user_email (str): The user's email address.
-            user_pwd (str): The user's password.
+            user_email (str): The email of the user to authenticate.
+            user_pwd (str): The password of the user to authenticate.
 
         Returns:
-            TypeVar('User'): The User instance if the credentials are valid,
-                             otherwise None.
+            TypeVar('User'): The `User` instance if the credentials are valid,
+                             otherwise `None`. Returns `None` if `user_email`
+                             or `user_pwd` are not strings, if no user is found
+                             with the email, or if the password is incorrect.
         """
         if user_email is None or not isinstance(user_email, str):
             return None
